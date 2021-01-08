107d5d8b8bf5db25e98575dbe4bce1f6ce2aa1e8
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-01-08 16:24:26 -0300

    - uncouple Entity in the Project MVC - Add Entit in Project Infra.Data

---
+++ CleanArch/CleanArch.Infra.Data/DependecyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Infra.Data.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.Data
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+
+            return services;
+        }
+    }
+}
