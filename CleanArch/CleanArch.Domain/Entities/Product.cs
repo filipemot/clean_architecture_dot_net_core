53baab8aed7b15670715c9e5cbdd6e6db57fb677
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-01-15 18:38:21 -0300

    - Create Entity Product - Change DependecyInjection to project CleanArchInfra.Ioc

---
+++ CleanArch/CleanArch.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public String Name { set; get; }
+        public String Description { set; get; }
+        public decimal Price { set; get; }
+
+    }
+}
