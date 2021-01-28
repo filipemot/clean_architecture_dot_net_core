42dc9a6b32356ac9347a081265a73f7f5861bc28
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-01-28 18:11:06 -0300

    Configuration AutoMapper

---
+++ CleanArch/CleanArch.MVC/MappingConfig/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CleanArch.Application.Mappings;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVC.MappingConfig
+{
+    public static class AutoMapperConfig
+    {
+        public static void AddAutoMapperConfiguration(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddAutoMapper(typeof(DomainToViewModelMappingProfile), 
+                typeof(ViewModelToDomainMappingProfile));
+        }
+    }
+}
